d59601c3080ed8a29e47628c049e1e0447d31924
xLsDg <xlsdg@qq.com>
2019-08-08 09:05:31 +0800

    chore(pkg): upgrade deps

--- config/config.js
+++ config/config.js
@@ -45,7 +45,7 @@ const plugins = [
       //   }
       // },
       hd: true,
-      fastClick: true,
+      fastClick: false,
       title: {
         defaultTitle: 'ANTD MOBILE',
         // format: '{parent}{separator}{current}',
